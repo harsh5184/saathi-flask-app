3a4f9121188fa3557fc43f5f00739891c800b551
Harsh Kumar <harsh5184264@gmail.com>
2024-09-24 14:56:31 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello from home"
+
+
+@app.route("/about")
+def about():
+    return "This is the about page."
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
